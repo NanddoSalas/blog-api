590a6e13a78f04a3a14d2d3ab5e1c5e36cfa0909
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-04-17 00:31:02 -0600

    str method overwritten on user model

--- apps/users/models/users.py
+++ apps/users/models/users.py
@@ -21,3 +21,7 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        """Return username."""
+        return self.username
