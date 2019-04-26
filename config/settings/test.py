07931be84a2372dc1ac2f6574968d387fb217495
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-04-26 13:25:06 -0600

    tests config file created

---
+++ config/settings/test.py
@@ -0,0 +1,30 @@
+"""
+Settings file for runing tests faster.
+"""
+
+from .base import *
+
+
+# Base
+DEBUG = False
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="7lEaACt4wsCj8JbXYgQLf4BmdG5QbuHTMYUGir2Gc1GHqqb2Pv8w9iXwwlIIviI2")
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
+
+# Cache
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": ""
+    }
+}
+
+
+# Passwords
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+
+# Email
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 1025
