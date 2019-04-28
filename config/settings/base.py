2195345f33bdf3b790eb6e65fcf4db00e8960436
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-04-27 21:04:21 -0600

    django admin and unused apps removed

--- config/settings/base.py
+++ config/settings/base.py
@@ -53,11 +53,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DJANGO_APPS = [
     'django.contrib.auth',
-    'django.contrib.admin',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 THIRD_PARTY_APPS = [
--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,9 @@
 # Django
 from django.urls import path, include
 from django.conf import settings
-from django.contrib import admin
+
 
 urlpatterns = [
-    path(settings.ADMIN_URL, admin.site.urls),
     path('', include('apps.users.urls')),
     path('', include('apps.posts.urls')),
 ]
