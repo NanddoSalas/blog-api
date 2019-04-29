21d7cb20b948c5e17fa01505e4c4630e5be82521
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-04-28 22:22:23 -0600

    PostFilter created

---
+++ apps/posts/filters.py
@@ -0,0 +1,21 @@
+"""Post Filters."""
+
+# Filters
+from django_filters import rest_framework as filter
+
+# Models
+from apps.posts.models import Post
+
+
+class PostFilter(filter.FilterSet):
+    """Filter by post's creator (username)."""
+
+    username = filter.CharFilter(field_name='user', lookup_expr='username')
+
+    class Meta:
+        """Meta Options."""
+
+        model = Post
+        fields = ('user',)
+
+# Please someone help me to understand how it works and why it works
