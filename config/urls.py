bda48c344e66dee46a677a24ecea82805380ccc5
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-05-10 10:26:37 -0600

    custom error handlers configured

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,18 @@
 from django.urls import path, include
 from django.conf import settings
 
+# Views
+from config.views import Handler403, Handler404
+
 
 urlpatterns = [
     path('', include('apps.users.urls')),
     path('', include('apps.posts.urls')),
 ]
+
+# Handdle Errors
+
+handler400 = 'rest_framework.exceptions.bad_request'
+handler403 = Handler403
+handler404 = Handler404
+handler500 = 'rest_framework.exceptions.server_error'
