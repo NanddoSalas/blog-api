1beb47fc9b9408706cbe1a52e5fab07521fa7754
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-05-17 13:24:02 -0600

    api_root view created

--- config/views.py
+++ config/views.py
@@ -5,6 +5,8 @@ from django.http import JsonResponse
 
 # Django REST Framework
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 def Handler403(request, exception):
@@ -25,3 +27,18 @@ def Handler404(request, exception):
         'error': 'Not Found (404)',
     }
     return JsonResponse(data=data, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def api_root(request):
+    """Show all Api links."""
+    return Response({
+        'signup': '/users/signup/',
+        'login': '/users/login/',
+        'verify': '/users/verify/',
+        'user': '/users/{username}/',
+        'posts': '/posts/',
+        'post': '/posts/{post_id}/',
+        'comments': '/posts/{post_id}/comments/',
+        'comment': '/posts/{post_id}/comments/{comment_id}/'
+    })
