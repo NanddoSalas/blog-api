7d16519232cb14d11cffe33c231868abc25963aa
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-06-28 23:59:41 -0600

    ViewSetTestUtilities class created
     - get_action_url shortcut created

--- apps/utilities.py
+++ apps/utilities.py
@@ -1,5 +1,8 @@
 """Utilities."""
 
+# Urls
+from django.urls import reverse
+
 
 class SerializerUtilities():
     """Serializer utilities.
@@ -12,3 +15,14 @@ class SerializerUtilities():
     def get_serializer(self, *args, **kwargs):
         """Return the serializer instance with given args (data, context, etc)"""
         return self.serializer_class(*args, **kwargs)
+
+
+class ViewSetTestUtilities():
+    """Shortcuts for write viewset test more easy."""
+
+    basename = None
+
+    def get_action_url(self, action, *args, **kwargs):
+        """Return the url based on action."""
+        viewname = '{}-{}'.format(self.basename, action)
+        return reverse(viewname, *args, **kwargs)
